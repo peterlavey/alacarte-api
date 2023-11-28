f6d0cef41da6f4d4a3a52370a3b657500a1f6d3c
Pedro Cisternas <pedrocisternasd@gmail.com>
2023-11-27 21:28:53 -0300

    fix: save file

--- src/routes/fileSystem.js
+++ src/routes/fileSystem.js
@@ -3,20 +3,15 @@ const fs = require("fs");
 const saveFile = (req, res) => {
     const data = JSON.stringify(req.body);
     try {
-        if (!fs.existsSync('data')) {
-            fs.mkdirSync('data');
-            fs.writeFileSync("data/cartes.json", data);
-            res.send(data);
-        } else {
-            res.send(err);
-        }
+        fs.writeFileSync("dist/data/cartes.json", data);
+        res.send(data);
     } catch (err) {
         res.send(err);
     }
 };
 
 const readFile = (req, res) => {
-    fs.readFile("data/cartes.json", (err, data) => {
+    fs.readFile("dist/data/cartes.json", (err, data) => {
         if (err)
             res.send(err);
         else {
